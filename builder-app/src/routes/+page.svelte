02997c840a43bdd069b0c234281e06de22015d9f
John Goh <johngohrw@gmail.com>
2024-02-28 01:45:44 +0800

    feat: add shadcn/ui

--- builder-app/src/routes/+page.svelte
+++ builder-app/src/routes/+page.svelte
@@ -1,10 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  let x = 0;
+
+  const handleClick = () => {
+    x += 1;
+  };
+</script>
+
+<div class="page-container">
+  <div class="builder-header">header</div>
+  <div class="builder-content">
+    <h1>Welcome to SvelteKit</h1>
+    d
+    <button on:click={handleClick}></button>
+    <p></p>
+  </div>
+</div>
 
 <style>
-  h1 {
-    color: red;
+  .page-container {
+    height: 100%;
+    position: relative;
+  }
+
+  .builder-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #333;
+    height: 40px;
+  }
+
+  .builder-content {
+    position: relative;
+    padding-top: 40px;
   }
 </style>
