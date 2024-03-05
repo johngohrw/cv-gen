7ee931a7956b168e60bf84edefd7535af8dd4df5
John Goh <johngohrw@gmail.com>
2024-03-05 15:25:04 +0800

    wip: form builder types

--- builder-app/src/routes/+page.svelte
+++ builder-app/src/routes/+page.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
-  let x = 0;
+  import type { ResumeData, ValidSection, Experience } from "../../../types";
 
-  const handleClick = () => {
-    x += 1;
+  let experienceSectionTemplate: ValidSection = {
+    title: "",
+    data: [],
+    type: "experienceList",
   };
+
+  let experienceDataTemplate: Experience = {
+    title: "",
+    subtitle: "",
+    location: "",
+    date: "",
+    items: [],
+  };
+
+  // type TypeName<T> = T extends string
+  //   ? "string"
+  //   : T extends number
+  //     ? "number"
+  //     : T extends boolean
+  //       ? "boolean"
+  //       : T extends undefined
+  //         ? "undefined"
+  //         : T extends Function
+  //           ? "function"
+  //           : "object";
+
+  // type Fields<Shape> = Record<Shape>;
+  // type Schema<Shape> = Record<"fields", Fields<Shape>> &
+  //   Record<"type", TypeName<Shape>>;
+
+  let formSchema: Schema<ResumeData> = {
+    fields: {
+      profile: {
+        name: "string",
+        caption: "string",
+        location: "string",
+        description: "string",
+        links: [],
+      },
+      sections: [],
+    },
+    type: "object",
+  };
+
+  const handleFormChange = () => {};
 </script>
 
 <div class="page-container">
   <div class="builder-header">header</div>
   <div class="builder-content">
     <h1>Welcome to SvelteKit</h1>
-    d
-    <button on:click={handleClick}></button>
-    <p></p>
+    <button on:click={() => {}}>huh</button>
+
+    <p>formstate {JSON.stringify(formState)}</p>
   </div>
 </div>
 
@@ -20,6 +62,7 @@
   .page-container {
     height: 100%;
     position: relative;
+    background: red;
   }
 
   .builder-header {
